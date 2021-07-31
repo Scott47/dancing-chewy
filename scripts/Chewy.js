b1b7859c5fc4de32a9081271990d3e77f8f8a8ae
Scott47 <scottsilver47@gmail.com>
2021-07-31 12:41:36 -0500

    gif data returns from api will render

--- scripts/Chewy.js
+++ scripts/Chewy.js
@@ -3,4 +3,6 @@ import { getChewyGifs } from "./database.js";
 export const Chewy = () => {
     const chewbaccaGifs = getChewyGifs()
     console.log(chewbaccaGifs)
+    const gif = chewbaccaGifs.map(chewyGif => `<div class="chewy--${chewyGif.id}"><img src="${chewyGif.media[0].tinygif.url}"/></div>`)
+    return gif.join("")
 }
--- scripts/database.js
+++ scripts/database.js
@@ -2,7 +2,50 @@ import { tenorBaseUrl, apiKey } from "../.Settings.js"
 
 const database = {
     dancetypes: [],
-    chewygifs: []
+    chewygifs: [],
+    moods: [
+        {
+            id: 1,
+            mood: "happy"
+        },
+        {
+            id: 2,
+            mood: "upset"
+        },
+        {
+            id: 3,
+            mood: "giddy"
+        },
+        {
+            id: 4,
+            mood: "extra"
+        },
+        {
+            id: 5,
+            mood: "chill"
+        },
+        {
+            id: 6,
+            mood: "frustrated"
+        },
+        {
+            id: 7,
+            mood: "demure"
+        },
+        {
+            id: 8,
+            mood: "fly"
+        },
+        {
+            id: 9,
+            mood: "bored"
+        },
+        {
+            id: 10,
+            mood: "special"
+        },
+
+    ]
 }
 
 export const fetchChewyGifs = () => {
