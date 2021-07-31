3f82934f4659d40ed6cf8979682931885916ed37
Scott47 <scottsilver47@gmail.com>
2021-07-31 15:56:34 -0500

    some gifs render based on mood selection

--- scripts/Chewy.js
+++ scripts/Chewy.js
@@ -1,7 +1,15 @@
+import { ChewyHTML } from "./ChewyHTML.js";
 import { getChewyGifs } from "./database.js";
 
-export const Chewy = () => {
-    const chewbaccaGifs = getChewyGifs()
-    const gif = chewbaccaGifs.map(chewyGif => `<div class="chewy--${chewyGif.id}"><img src="${chewyGif.media[0].tinygif.url}"/></div>`)
-    return gif.join("")
+// export const Chewy = () => {
+//     const chewbaccaGifs = getChewyGifs()
+//     console.log(chewbaccaGifs)
+//     const gif = chewbaccaGifs.map(chewyGif => `<div class="chewy--${chewyGif.id}"><img src="${chewyGif.media[0].tinygif.url}"/></div>`)
+//     return gif.join("")
+// }
+export const Chewy = (gifObject) => {
+    console.log(gifObject.media[0].tinygif.url)
+    return `<div class="chewy--${gifObject.id}"><img src="${gifObject.media[0].tinygif.url}"/></div>`
 }
+
+
--- scripts/MoodSelector.js
+++ scripts/MoodSelector.js
@@ -5,7 +5,7 @@ const mainContainer = document.querySelector("#container")
 export const MoodSelector = () => {
     const allMoods = getMoods()
     let moodHTML = "<select class='moods'><option value='0'>Select a mood...</option>"
-    const moodOptions = allMoods.map(mood => `<option value="${mood.id}">${mood.mood}</option>`)
+    const moodOptions = allMoods.map(mood => `<option value="${mood.mood}">${mood.mood}</option>`)
     moodHTML += moodOptions
     moodHTML += "</select>"
     return moodHTML
@@ -23,5 +23,3 @@ document.addEventListener("change", moodChangeEvent => {
     }
 })
 
-
-// Dispatch to event hub
